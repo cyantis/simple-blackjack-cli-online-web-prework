38bcd324291b2fe80a74e5c6b6d65988afa6fd49
Cody Yantis <cyantis@gmail.com>
2019-01-23 21:20:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ end
 def hit?(current_total)
   prompt_user
   input = get_user_input
-  while input != "s" || "h"
+  while input != "s" || input != "h"
     invalid_command
     prompt_user
     input = get_user_input
